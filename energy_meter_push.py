493240c891432a89a9b467f6fad3d074907c0a08
surajbeston <surajbeston@gmail.com>
2021-02-21 02:18:37 +0545

    added arduino code and cleaned raspberry code to work

--- energy_meter_push.py
+++ energy_meter_push.py
@@ -4,16 +4,21 @@ import time
 import json
 from web3 import Web3
 import threading
+import math
 
 infura_url = "https://kovan.infura.io/v3/51ee58fc0fa94ed29565e741714bf9ef"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
-contractAddress = "0x6D84B78870D93080C637a9B27B1Ebac3BE0d3A12"
+time.sleep(10)
+contractAddress = "0x9D945BC2e6d75eB304988E68756Dd981AC927651"
 
-abi_file = open("/home/pi/power.json")
-abi = json.loads(abi_file.read())
+abi_file = open("power.json")
+abi_raw = abi_file.read()
+abi = json.loads(abi_raw)
+
 
 contract = web3.eth.contract(address=contractAddress, abi=abi)
+time.sleep(10)
 
 control_available = True
 lot = 0
@@ -23,20 +28,24 @@ plant_value = 0
 distributor_value = 0
 consumer_value = 0
 
+plant_value_prerecord = 0
+distributor_value_prerecord = 0
+consumer_value_prerecord = 0
+
 def plant():
     print ("MINING FOR PLANT STARTED")
     public_key = "0x172CdCE74e56544df20138Fc721E1932603f9E0f"
     private_key = "c6349e6e99e29575f024c93ce77053a910ab1f94a24df3ad6644cb3ae08ac405"
-
+    print ("reached here1")
     nonce = web3.eth.getTransactionCount(public_key)
-
     transaction = contract.functions.mineTokenPlant(
-        200, "0xEDe0a403609bfDc96b5294436CDb51A4F1bD94C7").buildTransaction({
+        math.floor(plant_value_prerecord), "0xEDe0a403609bfDc96b5294436CDb51A4F1bD94C7").buildTransaction({
             'gas': 8000001,
             'from': public_key,
             'nonce': nonce
         })
     signed_txn = web3.eth.account.signTransaction(transaction, private_key=private_key)
+    print("reached here")
     web3.eth.sendRawTransaction(signed_txn.rawTransaction)
     print ("MINING FOR PLANT ENDED")
 
@@ -48,7 +57,7 @@ def distributor():
     nonce = web3.eth.getTransactionCount(public_key)
 
     transaction = contract.functions.evaluateTokenDistributor(
-        180, "0xedB1DB9cb450515b28c5518B333C577994586Da5", "0x172CdCE74e56544df20138Fc721E1932603f9E0f").buildTransaction({
+        math.floor(distributor_value_prerecord), "0xedB1DB9cb450515b28c5518B333C577994586Da5", "0x172CdCE74e56544df20138Fc721E1932603f9E0f").buildTransaction({
         'gas': 7000000,
         'value': 10**11,
         'from': public_key,
@@ -65,7 +74,7 @@ def consumer():
     nonce = web3.eth.geAtTransactionCount(public_key)
 
     transaction = contract.functions.evaluateTokenConsumer(
-        160, "0xEDe0a403609bfDc96b5294436CDb51A4F1bD94C7").buildTransaction({
+        math.floor(consumer_value_prerecord), "0xEDe0a403609bfDc96b5294436CDb51A4F1bD94C7").buildTransaction({
         'gas': 7000000,
         'value': 10**11,
         'from': public_key,
@@ -84,15 +93,17 @@ def readSerial(event):
             val = sercom.readline().decode('utf-8')
             val = val.split(" ")
             try:
-                if (val[1] == "plant\n"):
-                    plant_value += float(val[0])
-                elif (val[1] == "distributor\n"):
-                    distributor_value += float(val[0])
-                else :
-                    consumer_value += float(val[0])
+                try:
+                    if (val[1] == "plant\n"):
+                        plant_value += float(val[0])
+                    elif (val[1] == "distributor\n"):
+                        distributor_value += float(val[0])
+                    else :
+                        consumer_value += float(val[0])
+                except IndexError:
+                    pass
             except ValueError:
                 pass
-
 
 def handle_event(event):
     receipt = web3.eth.waitForTransactionReceipt(event['transactionHash'])
@@ -114,6 +125,8 @@ def handle_event(event):
         if entity == 1:
             distributor()
         elif entity == 2:
+            print ("reached here to consumer")
+            time.sleep(5)
             consumer()
         control_available = True            
 
@@ -122,15 +135,29 @@ def log_loop(event_filter, poll_interval):
         for event in event_filter.get_new_entries():
             handle_event(event)
             time.sleep(poll_interval)
-        
+
+def start_lot(event):
+    global consumer_value_prerecord, plant_value_prerecord, distributor_value_prerecord
+    while True:
+        print ("Consumer Value", consumer_value)
+        print ("Plant Value", plant_value)
+        print ("Distributor Value", distributor_value)
+
+        consumer_value_prerecord, plant_value_prerecord, distributor_value_prerecord = consumer_value, plant_value, distributor_value
+        plant()
+
+        time.sleep(120)
+
 sercom = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 sercom.flush()
 
 event = threading.Event()
 
-thread = threading.Thread(target = readSerial, args = (event,))
+thread1 = threading.Thread(target = readSerial, args = (event,))
+thread2 = threading.Thread(target = start_lot, args = (event,))
 
-thread.start()
+thread1.start()
+thread2.start()
 
-# block_filter = web3.eth.filter({'fromBlock':'latest', 'address':contractAddress})
-# log_loop(block_filter, 2)
+block_filter = web3.eth.filter({'fromBlock':'latest', 'address':contractAddress})
+log_loop(block_filter, 2)
---
+++ sensorDataPull.ino
@@ -0,0 +1,171 @@
+//#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
+
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+unsigned long plantTime = 0;
+unsigned long plantTime1 = 0;
+unsigned long plantTime2 = 0;
+
+const int plantVoltagePin = A0;
+const int plantCurrentPin = A1;
+
+const int distributorVoltagePin = A2;
+const int distributorCurrentPin = A3;
+
+const int consumerVoltagePin = A4;
+const int consumerCurrentPin = A5;
+
+float plantVoltage = 0;
+float plantCurrent = 0;
+
+float distributorVoltage = 0;
+float distributorCurrent = 0;
+
+float consumerVoltage = 0;
+float consumerCurrent = 0;
+
+float inputVoltVal = 0;
+
+int inputCurrentVal = 0;
+
+float totalCurrentVal = 0;
+
+float plantPower = 0;
+float energy1 = 0;
+float plantEnergy = 0;
+
+float distributorPower = 0;
+float energy2 = 0;
+float distributorEnergy = 0;
+
+float consumerPower = 0;
+float energy3 = 0;
+float consumerEnergy = 0;
+
+
+void setup()
+{
+  //lcd.init();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  plantTime1 = millis();
+  
+  inputVoltVal = analogRead(plantVoltagePin);
+  plantVoltage = (inputVoltVal/1023.0)*24.5;
+  
+  for (int i = 0; i < 100; i++)
+  {
+    totalCurrentVal += analogRead(plantCurrentPin);
+    delay(3);
+  }
+  
+  inputCurrentVal = totalCurrentVal/100;
+  totalCurrentVal = 0;
+  
+  plantCurrent = (inputCurrentVal * 5000.0) / 1024.0;
+  plantCurrent -= 2509.5; //- plantCurrent;
+  plantCurrent /= 0.185;
+  
+  plantPower = plantCurrent * plantVoltage;
+  
+  
+  
+  // Start distributor
+  inputVoltVal = 0;
+  inputCurrentVal = 0;
+  
+  inputVoltVal = analogRead(distributorVoltagePin);
+  distributorVoltage = (inputVoltVal/1023.0)*24.5;
+  
+  for (int i = 0; i < 100; i++)
+  {
+    totalCurrentVal += analogRead(distributorCurrentPin);
+    delay(3);
+  }
+  
+  inputCurrentVal = totalCurrentVal/100;
+  totalCurrentVal = 0;
+  
+  distributorCurrent = (inputCurrentVal * 5000.0) / 1024.0;
+  distributorCurrent -= 2520; //- plantCurrent;
+  distributorCurrent /= 0.185;
+  
+  distributorPower = distributorCurrent * distributorVoltage;
+  
+  
+  
+  // Start consumer
+  inputVoltVal = 0;
+  inputCurrentVal = 0;
+  
+  inputVoltVal = analogRead(consumerVoltagePin);
+  consumerVoltage = (inputVoltVal/1023.0)*24.5;
+  
+  for (int i = 0; i < 100; i++)
+  {
+    totalCurrentVal += analogRead(consumerCurrentPin);
+    delay(3);
+  }
+  
+  inputCurrentVal = totalCurrentVal/100;
+  totalCurrentVal = 0;
+  
+  consumerCurrent = (inputCurrentVal * 5000.0) / 1024.0;
+  consumerCurrent -= 2494; //- plantCurrent;
+  consumerCurrent /= 0.185;
+  
+  consumerPower = consumerCurrent * consumerVoltage;
+  
+  
+  // Calculate all Energy ang send through serial:\
+  
+  plantTime2 = millis();
+  plantTime = plantTime2 - plantTime1;
+  
+  energy1 = (plantPower * plantTime) / 360000;
+  
+  if( energy1 < 0 )
+  {
+    energy1 = 0.00;
+  }
+  
+  plantEnergy += energy1;
+  
+  
+  energy2 = (distributorPower * plantTime) / 360000;
+  
+  if( energy2 < 0 )
+  {
+    energy2 = 0.00;
+  }
+  
+  distributorEnergy += energy2;
+  
+  
+  energy3 = (consumerPower * plantTime) / 360000;
+  
+  if( energy3 < 0 )
+  {
+    energy3 = 0.00;
+  }
+  
+  consumerEnergy += energy3;
+  
+  
+  Serial.print(energy1);
+  Serial.print(" plant\n");
+  Serial.print(energy2);
+  Serial.print(" distributor\n");
+  Serial.print(energy3);
+  Serial.print(" consumer\n");
+  Serial.println(plantTime);
+//  lcd.setCursor(0,0);
+//  lcd.print("Plant: ");
+//  lcd.print(plantEnergy);
+  
+//  delay(1000);
+}
